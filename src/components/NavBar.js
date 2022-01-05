bf623a84f92fff992ed5604ab361f5d319778f81
the company <thecompany001@protonmail.com>
2022-01-04 19:08:34 -0500

    Navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Button } from "./Button";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
@@ -19,13 +19,17 @@ function Navbar() {
     }
   };
 
+  useEffect(() => {
+    showButton()
+  }, []);
+
   window.addEventListener("resize", showButton);
 
   return (
     <>
       <nav className="navbar">
         <div className="navbar-container">
-          <Link to="/" className="navbar-logo">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
             TRVL <i className="fab fa-typo3" />
           </Link>
           <div className="menu-icon" onClick={handleClick}>
@@ -53,6 +57,15 @@ function Navbar() {
                 onClick={closeMobileMenu}
               >
                 Products
+              </Link>
+            </li>
+            <li>
+              <Link
+                to='/sign-up'
+                className='nav-links-mobile'
+                onClick={closeMobileMenu}
+              >
+                Sign Up
               </Link>
             </li>
           </ul>
