458bdacad5bb815b61fa44ad112a7a2455450fb9
the company <thecompany001@protonmail.com>
2022-01-05 20:02:02 -0500

    Pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Switch } from 'react-router-dom'
 import './App.css';
 import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 
 function App() {
@@ -13,6 +16,9 @@ function App() {
     	<Navbar />
       	<Switch>
         	<Route path='/' exact component={Home} />
+        	<Route path='/services' exact component={Services} />
+        	<Route path='/products' exact component={Products} />
+        	<Route path='/sign-up' exact component={SignUp} />
       	</Switch>
     </Router>
     </>
